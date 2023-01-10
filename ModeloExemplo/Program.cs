e380f76b33b2def6874affdfa938d109065ba604
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-01-10 19:03:26 -0300

    Modelo Anemico

---
+++ ModeloExemplo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Text.Json;
+using System.Web.Helpers;
+
+namespace ModeloAnemicoVsEnrriquecido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+
+            cliente.Id = 1;
+            cliente.Nome = "Jackson";
+
+            Console.WriteLine(JsonSerializer.Serialize(cliente));
+        }
+    }
+}
