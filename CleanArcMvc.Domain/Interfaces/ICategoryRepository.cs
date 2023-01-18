35bee72c0ee3d35f90cb9b2745e802389a0141c8
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-01-18 18:18:46 -0300

    Criando as interfaces dos repositórios

---
+++ CleanArcMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArcMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        //Task<> - Defines as Asynchronous
+        //IEnumerable<> - Defines as List of one or more records
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ CleanArcMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArcMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        //Task<> - Defines as Asynchronous
+        //IEnumerable<> - Defines as List of one or more records
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
