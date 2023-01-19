b456696a3fc69d81505dc2d542b082aebf7e95b6
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-01-19 13:13:40 -0300

    Implementando o Repositório - CategoryRepository

---
+++ CleanArcMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Domain.Interfaces;
+using CleanArcMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _categoryContext;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _categoryContext.Categories.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _categoryContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _categoryContext.Categories.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _categoryContext.Entry(category).State = EntityState.Modified;
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
