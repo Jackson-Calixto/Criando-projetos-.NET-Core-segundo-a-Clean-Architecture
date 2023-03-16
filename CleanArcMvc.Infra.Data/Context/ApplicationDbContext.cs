308d88d97bb31997be0eb7ea7e447e35fd0222e8
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-03-16 07:24:57 -0300

    EntityFrameworkCore\Add-Migration

--- CleanArcMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArcMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,14 +1,11 @@
 ﻿using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArcMvc.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Criando-projetos-.NET-Core-segundo-a-Clean-Architecture
+
 Criando projetos .NET Core segundo a Clean Architecture
+
+# Add-Migration and Update-Database with Identity
+
+EntityFrameworkCore\Add-Migration
+EntityFrameworkCore\Update-Database
+Com Identity não funciona sem a referencia EntityFrameworkCore\
