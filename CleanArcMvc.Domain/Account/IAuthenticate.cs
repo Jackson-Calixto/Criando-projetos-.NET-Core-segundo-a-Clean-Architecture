e9b8b741bdc48e62a5a0d1d7a462ed919f2076dd
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-03-20 05:25:05 -0300

    Criando as interfaces na camada Domain

---
+++ CleanArcMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Domain.Account
+{
+  public interface IAuthenticate
+  {
+    Task<bool> Authenticate(string email, string password);
+    Task<bool> ResisterUser(string email, string password);
+    Task Logout();
+  }
+}
