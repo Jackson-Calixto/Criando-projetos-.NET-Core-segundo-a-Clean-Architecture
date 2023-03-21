57344c3d4f17385ca6a1eba94bde5fa9833c88e2
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-03-21 04:34:38 -0300

    Criando o controlador AccountController e as ViewModels na camada WebUI

---
+++ CleanArcMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using CleanArcMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArcMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+
+        public AccountController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+    }
+}
