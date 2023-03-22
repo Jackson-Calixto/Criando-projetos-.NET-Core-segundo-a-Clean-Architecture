3cfdc4f9dc0734b3362eb64762446c202fa599f8
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-03-22 05:29:42 -0300

    HttpGet - Retornando todas as Categorias

--- CleanArcMvc.API/Controllers/CategoriesController.cs
+++ CleanArcMvc.API/Controllers/CategoriesController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+using CleanArcMvc.Application.DTOs;
+using CleanArcMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArcMvc.API.Controllers
 {
@@ -7,5 +11,22 @@ namespace CleanArcMvc.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Category not found");
+            }
+            return Ok(categories);
+        }
     }
 }
--- CleanArcMvc.API/Startup.cs
+++ CleanArcMvc.API/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArcMvc.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
