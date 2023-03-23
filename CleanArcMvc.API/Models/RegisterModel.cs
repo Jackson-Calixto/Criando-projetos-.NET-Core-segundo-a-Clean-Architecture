d2d029354e376eec237d58b01e367ef008a0d286
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2023-03-23 04:49:47 -0300

    Configurando as informações do token e os DTOs usados

---
+++ CleanArcMvc.API/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArcMvc.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
